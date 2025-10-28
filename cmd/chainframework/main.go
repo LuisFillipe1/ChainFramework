0ce6f9f75a30d13cc2fd936d99b7f191b25632cb
Luis <43860241+LuisFillipe1@users.noreply.github.com>
2025-10-29 01:22:16 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/chainframework/main.go
@@ -0,0 +1,23 @@
+// cmd/chainframework/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "chainframework/internal/chainframework"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := chainframework.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
